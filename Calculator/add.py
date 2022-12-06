32aed40a383423e5457450ae1a5c8fe2fdf4b283
kamal ghimire <kamalghimire689@gmail.com>
2022-12-06 07:34:29 +0545

    this is my first commit

---
+++ Calculator/add.py
@@ -0,0 +1,7 @@
+# this is a addition program for adding the number
+def add(*arg1):
+    print("type of operation is:",arg1)
+    print(arg1[1]+arg1[2])
+
+add("addition",2,3)
+    
---
+++ Calculator/div.py
@@ -0,0 +1,8 @@
+#this is for division part
+def div(*args):
+    print("operation type = ", args[0])
+    if(args[1] == 0):
+        print("operation cant be performed")
+    else:
+       print(args[1]//args[2])
+div("division",6,3)
